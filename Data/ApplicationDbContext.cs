9a00233ed57631d077063b503df756f83bb63617
Mosquera31 <leison3131@hotmail.com>
2024-10-07 08:03:43 -0500

    Database has been connected

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Reserva.Models;
+
+namespace Reserva.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Guest> Guests { get; set; }
+    public DbSet<Room_type> Room_Types { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+
+
+public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base (options){}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,25 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using Reserva.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//load the enviroment variables
+
+Env.Load(); // callin all the variables
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbDatabase = Environment.GetEnvironmentVariable("DB_DATABASE");
+var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+
+var ConnectionDB = $"server={dbHost}; port={dbPort}; database={dbDatabase}; uid={dbUser}; password={dbPassword}";
+builder.Services.AddDbContext<ApplicationDbContext>(Options => Options.UseMySql(ConnectionDB, ServerVersion.Parse("8.0.20-mysql")));
+
+
+
+
 
 // Add services to the container.
 
